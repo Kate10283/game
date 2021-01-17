b71b4baaf135d8bb17b2aa905b0ef0f6f184301e
Kate <katya.10283@gmail.com>
2021-01-17 19:19:00 +0200

    last version

--- script.js
+++ script.js
@@ -3,39 +3,46 @@ var growingPlatform = document.querySelector(".groving-platform");
 var newPlatform = document.querySelector(".new-platform");
 var platform = document.querySelector(".platform");
 var bird = document.querySelector(".bird");
-var body = document.body;
+var body = document.querySelector(".layout");
 var heightGrowingPlatform = 0;
 var distance = 0;
 var widthNewPlatform = 0;
-// расстояние для перехода на следующую платформу
-var distanceForBird = 0;
-var timerId = 0;
-
-// body.addEventListener("mousedown", grow);
-// body.addEventListener("mouseup", stopGrow);
+var distanceForBird = 0;  // расстояние для перехода на следующую платформу
+var timerId1 = 0;
+var timerId2 = 0;
+var backgroundPosition = ["-130px 0", "-255px 0", "-380px 0", "-505px 0", "-630px 0",
+    "-760px 0", "0 0"]; // позиции для спрайта
+var widthBird = Number(getComputedStyle(bird).width.slice(0, getComputedStyle(bird).width.length - 2));
+var lifes = document.querySelector(".lifes");
+var counterLifes = 3;
+var score = document.querySelector(".score");
+var counterScore = 0;
+var counterCalcScore = 0; // аналогично количеству пройденных уровней
+var record = document.querySelector(".record");
+var allRecords = [];
 
 body.onmousedown = grow;
 body.onmouseup = stopGrow;
 
 function grow() {
-    timerId = window.setInterval(() => {
+    timerId1 = window.setInterval(() => {
         if (!growingPlatform.style.height) {
-            // console.log("heightGrowingPlatform before 0 " + heightGrowingPlatform);
             growingPlatform.style.height = "10px";
             heightGrowingPlatform = 10;
         }
+        else if (Number(growingPlatform.style.height.slice(0, growingPlatform.style.height.length - 2)) >= 619) {
+            stopGrow();
+        }
         else {
-            // console.log("heightGrowingPlatform " + heightGrowingPlatform);
             let height = growingPlatform.style.height.slice(0, growingPlatform.style.height.length - 2);
             growingPlatform.style.height = Number(height) + 10 + "px";
             heightGrowingPlatform += 10;
         }
-        // console.log("px: " + body.clientHeight * heightGrowingPlatformVH / 100);
     }, 15);
 }
 
 function stopGrow() {
-    window.clearInterval(timerId);
+    window.clearInterval(timerId1);
     body.onmousedown = null;
     body.onmouseup = null;
     fallDown();
@@ -43,36 +50,17 @@ function stopGrow() {
 
 function fallDown() {
     growingPlatform.style.transform = "rotate(90deg)";
-    // переводим падающую платформу из vh в vw
-    // heightGrowingPlatform = body.clientHeight * heightGrowingPlatformVH / body.clientWidth;
-    // run();
     checkAndRun();
 }
 
-// function run() {
-//     window.setTimeout(() => {
-//         bird.style.transform = "translate(5vw, -1.4vw)";
-//         bird.style.transition = "transform 0.5s linear";
-//     }, 1000);
-//     window.setTimeout(() => {
-//         bird.style.transform = "translate(" + heightGrowingPlatformVH + "vh, -1.4vw)";
-//         bird.style.transition = "transform 1s linear";
-//     }, 1500);
-//     check();
-// }
-
 function checkAndRun() {
     distance = Number(newPlatform.style.marginLeft.slice(0, newPlatform.style.marginLeft.length - 2));
-    console.log("distance (marginLeft) = " + distance);
+    // console.log("distance (marginLeft) = " + distance);
     widthNewPlatform = Number(newPlatform.style.width.slice(0, newPlatform.style.width.length - 2));
-    // console.log("newPlatform = " + newPlatform.style.width);
-    console.log("widthNewPlatform = " + widthNewPlatform);
-    console.log("heightGrowingPlatform = " + heightGrowingPlatform);
-    // bird.style.backgroundImage = "url(/images/run.png)";
-    // bird.style.backgroundPosition = "-496px 0";
-    // bird.style.backgroundSize = "cover";
+    // console.log("widthNewPlatform = " + widthNewPlatform);
+    // console.log("heightGrowingPlatform = " + heightGrowingPlatform);
     if (heightGrowingPlatform >= distance && heightGrowingPlatform <= distance + widthNewPlatform) {
-        console.log("you win");
+        // console.log("you win");
         // птичка поднимается
         window.setTimeout(() => {
             bird.style.transform = "translate(0, -24px)";
@@ -81,12 +69,14 @@ function checkAndRun() {
         // птичка бежит по вырастающей платформе (в конец новой платформы)
         window.setTimeout(() => {
             distanceForBird = Number(distance) + Number(widthNewPlatform);
-            console.log("distanceForBird = " + distanceForBird);
+            // console.log("distanceForBird = " + distanceForBird);
             bird.style.transform = "translate(" + distanceForBird + "px, -24px)";
             bird.style.transition = "transform 1s linear";
+            run();
         }, 1500);
         // птичка опускается на новую платформу
         window.setTimeout(() => {
+            stopRun();
             growingPlatform.style.opacity = "0";
             // let distanceForBird = Number(distance) + Number(widthNewPlatform);
             bird.style.transform = "translate(" + distanceForBird + "px, 0)";
@@ -99,11 +89,12 @@ function checkAndRun() {
             growingPlatform.style.opacity = "1";
             growingPlatform.style.left = (widthNewPlatform - 25) + "px";
             heightGrowingPlatform = 0;
+            calcScore();
             shift();
         }, 3000);
     }
     else {
-        console.log("you lose");
+        // console.log("you lose");
         // птичка поднимается
         window.setTimeout(() => {
             bird.style.transform = "translate(0, -24px)";
@@ -111,12 +102,14 @@ function checkAndRun() {
         }, 1000);
         // птичка бежит по вырастающей платформе
         window.setTimeout(() => {
-            bird.style.transform = "translate(" + (heightGrowingPlatform + 100) + "px, -24px)";
+            bird.style.transform = "translate(" + (heightGrowingPlatform + widthBird) + "px, -24px)";
             bird.style.transition = "transform 1s linear";
+            run();
         }, 1500);
         // птичка падает
         window.setTimeout(() => {
-            bird.style.transform = "translate(" + (heightGrowingPlatform + 100) + "px, 100px)";
+            stopRun();
+            bird.style.transform = "translate(" + (heightGrowingPlatform + widthBird) + "px, 100px)";
             bird.style.transition = "transform 0.3s ease-in";
         }, 2500);
         // начинаем все заново
@@ -126,6 +119,30 @@ function checkAndRun() {
             growingPlatform.style.height = "0";
             growingPlatform.style.transform = "rotate(0deg)";
             heightGrowingPlatform = 0;
+            counterLifes--;
+            switch (counterLifes) {
+                case 3:
+                    lifes.style.width = "120px";
+                    break;
+                case 2:
+                    lifes.style.width = "80px";
+                    break;
+                case 1:
+                    lifes.style.width = "40px";
+                    break;
+                default:
+                    alert("You lose");
+                    allRecords.push(counterScore);
+                    // console.log("counterScore " + counterScore);
+                    // console.log("allRecords.length " + allRecords.length);
+                    // console.log("allRecords[0] " + allRecords[0]);
+                    record.innerHTML = getMaxOfArray(allRecords);
+                    score.innerHTML = "0";
+                    counterLifes = 3;
+                    lifes.style.width = "120px";
+                    counterCalcScore = 0;
+                    counterScore = 0;
+            }
             body.onmousedown = grow;
             body.onmouseup = stopGrow;
         }, 2800);
@@ -137,29 +154,28 @@ function shift() {
     distance = Number(newPlatform.style.marginLeft.slice(0, newPlatform.style.marginLeft.length - 2));
     platform.style.opacity = "0";
     platform.style.transition = "opacity 0s linear";
-    
+
     var widthOldPlatform = Number(getComputedStyle(platform).width.slice(0, getComputedStyle(platform).width.length - 2));
-    console.log("widthOldPlatform: " + widthOldPlatform);
-    console.log("distance: " + distance);
-    console.log("distance + widthOldPlatform: " + (distance + widthOldPlatform));
+    // console.log("widthOldPlatform: " + widthOldPlatform);
+    // console.log("distance: " + distance);
+    // console.log("distance + widthOldPlatform: " + (distance + widthOldPlatform));
     let temp = distance + widthOldPlatform;
-    // let temp = 830;
     newPlatform.style.transform = "translate(-" + temp + "px, 0)";
     newPlatform.style.transition = "transform 1s linear";
 
-    
+
     bird.style.transform = "translate(0, 0)";
     bird.style.transition = "transform 1s linear";
 
     window.setTimeout(() => {
-        
+
         platform.style.visibility = "visible";
         platform.style.opacity = "1";
         platform.style.transition = "transform 0s linear";
-        
+
         newPlatform.style.opacity = "0";
         newPlatform.style.transition = "opacity 0s linear";
-        
+
     }, 1000);
     window.setTimeout(() => {
         newPlatform.style.visibility = "hidden";
@@ -168,8 +184,8 @@ function shift() {
 }
 
 function renderNewPlatform() {
-    newPlatform.style.width = rand(100, 280) + "px";
-    newPlatform.style.marginLeft = rand(100, 550) + "px";
+    newPlatform.style.width = rand(widthBird, 280) + "px";
+    newPlatform.style.marginLeft = rand(widthBird, 550) + "px";
     newPlatform.style.transform = "translate(0,0)";
     newPlatform.style.transition = "transform 1s linear";
     window.setTimeout(() => {
@@ -183,4 +199,48 @@ function renderNewPlatform() {
 
 function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function run() {
+    let counter = 0;
+    bird.style.backgroundImage = "url(/images/run.png)";
+    timerId2 = window.setInterval(() => {
+        bird.style.backgroundPosition = backgroundPosition[counter];
+        counter++;
+        if (counter > 6) {
+            counter = 0;
+        }
+    }, 190);
+}
+
+function stopRun() {
+    bird.style.backgroundImage = "";
+    bird.style.backgroundPosition = "";
+    window.clearInterval(timerId2);
+}
+
+function calcScore() {
+    counterCalcScore++;
+    if (counterCalcScore >= 1 && counterCalcScore <= 20) {
+        counterScore += 10;
+    }
+    else if (counterCalcScore >= 21 && counterCalcScore <= 40) {
+        counterScore += 15;
+    }
+    else if (counterCalcScore >= 41 && counterCalcScore <= 60) {
+        counterScore += 20;
+    }
+    else if (counterCalcScore >= 61 && counterCalcScore <= 80) {
+        counterScore += 30;
+    }
+    else if (counterCalcScore >= 81 && counterCalcScore <= 100) {
+        counterScore += 40;
+    }
+    else {
+        counterScore += 50;
+    }
+    score.innerHTML = counterScore;
+}
+
+function getMaxOfArray(list) {
+    return list.reduce((a, b) => a > b ? a : b);
 }
