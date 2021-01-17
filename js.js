474b02a97062e2c561db1b3e848a8c03b5059695
Kate10283 <77011490+Kate10283@users.noreply.github.com>
2021-01-17 19:20:07 +0200

    Delete js.js

--- js.js
+++
@@ -1,5 +0,0 @@
-var newPlatform = document.querySelector(".new-platform");
-var platform = document.querySelector(".platform");
-
-console.log(newPlatform.style.width);
-console.log(platform.style.width);
